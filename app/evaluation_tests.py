47c1ba4140daee5353434944114e09dc14ddd186
Johnny <johnnyallenyxc@gmail.com>
2024-07-19 10:43:11 +0100

    Roll back anything for debug

--- app/evaluation_tests.py
+++ app/evaluation_tests.py
@@ -5,41 +5,36 @@ try:
 except ImportError:
     from evaluation import Params, evaluation_function
 
+
 class TestEvaluationFunction(unittest.TestCase):
     """
     TestCase Class used to test the algorithm.
     ---
     Tests are used here to check that the algorithm written
     is working as it should.
-
     It's best practise to write these tests first to get a
     kind of 'specification' for how your algorithm should
     work, and you should run these tests before committing
     your code to AWS.
-
     Read the docs on how to use unittest here:
     https://docs.python.org/3/library/unittest.html
-
     Use evaluation_function() to check your algorithm works
     as it should.
     """
 
-    def test_general_eval(self):
-        response1 = """
-print(5)
-"""
-        response2 = """
-for i in range(5):
-    if i == 3:
-        print(i)
-"""
-        result1 = evaluation_function(response1, response1,
-                                      Params(is_unique_answer=True, is_multiple_answers=False, is_ai_feedback=False, has_output=True))
-        result2 = evaluation_function(response2, "",
-                                      Params(is_unique_answer=True, is_multiple_answers=False, is_ai_feedback=False, has_output=True))
-        assert result1['is_correct']
-        print(result1['feedback'])
-        assert not result2['is_correct']
+    def test_returns_is_correct_true(self):
+        response, answer, params = None, None, Params()
+        result = evaluation_function(response, answer, params)
+
+    def test_contains_if_statement(self):
+        code_string = "if x > 5:\n    print('x is greater than 5')"
+        result = evaluation_function(code_string, '', '')
+        self.assertEqual(result.get("is_correct"), True)
+
+    def test_contains_no_if_statement(self):
+        code_string = "print('Hello, world!')"
+        result = evaluation_function(code_string, '', '')
+        self.assertEqual(result.get("is_correct"), False)
 
 
 if __name__ == "__main__":
